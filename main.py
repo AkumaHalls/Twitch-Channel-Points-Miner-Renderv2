25fbf7560bee952c1ed7d0ceeb2cd6c88d36303a
AkumaHalls <153091442+AkumaHalls@users.noreply.github.com>
2024-05-26 18:22:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ twitch_miner = TwitchChannelPointsMiner(
             BET_wiN=Fore.MAGENTA                # Color allowed are: [BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET].
         ),
         telegram=Telegram(                                                          # You can omit or set to None if you don't want to receive updates on Telegram
-            chat_id=         ,                                                      # Chat ID to send messages @getmyid_bot
+            chat_id=123456789,                                                      # Chat ID to send messages @getmyid_bot
             token="123456789:shfuihreuifheuifhiu34578347",                          # Telegram API token @BotFather
             events=[Events.STREAMER_ONLINE, Events.STREAMER_OFFLINE,
                     Events.BET_LOSE, Events.CHAT_MENTION],                          # Only these events will be sent to the chat
